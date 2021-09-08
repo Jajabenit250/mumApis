0e2916567893687055cff6744908d78979d5f962
Jajabenit250 <jajabenit@gmail.com>
2021-09-08 04:01:10 +0200

    project initialization

---
+++ src/main/java/com/example/mumApis/MumApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.mumApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumApisApplication.class, args);
+	}
+
+}
